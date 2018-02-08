3ba5656c1c8b1a2fddabdf695f188dbfa4ab9af6
Desiree Amling <d.amling@tarent.de>
2018-02-08 13:11:13 +0100

    [TCF-112] Added functionality that starts a MessageVerticle to publish messages to the server without waiting on a reply.

---
+++ src/test/java/de/tarent/telekom/cot/mqtt/MessageIT.java
@@ -0,0 +1,67 @@
+package de.tarent.telekom.cot.mqtt;
+
+
+import de.tarent.telekom.cot.mqtt.util.MQTTTestServer;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Properties;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(VertxUnitRunner.class)
+public class MessageIT {
+
+    static Logger logger = LoggerFactory.getLogger(MessageIT.class);
+    MQTTHelper helper;
+    MQTTTestServer server;
+    Vertx vertx;
+
+    @Before
+    public void before() {
+        helper = MQTTHelper.getInstance();
+        vertx = helper.getVertx();
+        server = new MQTTTestServer();
+        vertx.deployVerticle(server);
+    }
+
+    @After
+    public void after() {
+        Set<String> list = vertx.deploymentIDs();
+        if (list != null && list.size() > 0) {
+            list.forEach(id -> {
+                logger.info("to undeploy:" + id);
+                vertx.undeploy(id);
+            });
+        }
+    }
+
+    @Test
+    public void testPublishMessage(final TestContext context) {
+        Properties prop = new Properties();
+        prop.setProperty("user", "publishMessage");
+        prop.setProperty("password", "somePassword");
+        prop.setProperty("brokerURI", "localhost");
+        prop.setProperty("brokerPort", "1883");
+        final String topic = "/ms/testDevice";
+        final String message = "test1234567890ab";
+        final Async async = context.async();
+        helper.publishMessage(topic, message, prop, back -> {
+            logger.info("Back:" + back);
+            assertTrue(back.toString().contains("published"));
+            async.complete();
+        });
+
+        async.awaitSuccess(3000);
+    }
+
+}
