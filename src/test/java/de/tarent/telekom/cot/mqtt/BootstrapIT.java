ef6c67994d7b134c55af417a93f5e169032c1013
Desiree Amling <d.amling@tarent.de>
2018-02-22 14:10:04 +0100

    [TEL-378] Code formatting after merge.

--- src/test/java/de/tarent/telekom/cot/mqtt/BootstrapIT.java
+++ src/test/java/de/tarent/telekom/cot/mqtt/BootstrapIT.java
@@ -3,16 +3,12 @@ package de.tarent.telekom.cot.mqtt;
 
 import de.tarent.telekom.cot.mqtt.util.MQTTTestClient;
 import de.tarent.telekom.cot.mqtt.util.MQTTTestServer;
-import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import io.vertx.mqtt.MqttClient;
-import io.vertx.mqtt.MqttClientOptions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -60,16 +56,16 @@ public class BootstrapIT {
         prop.setProperty("message", "test1234567890ab");
         String devId = "testDevice";
         Async async = context.async();
-        helper.registerDevice(devId, prop,back ->{
-            logger.info("Back:"+back);
-            context.assertTrue(((String)back).contains("status"));
+        helper.registerDevice(devId, prop, back -> {
+            logger.info("Back:" + back);
+            context.assertTrue(((String) back).contains("status"));
             async.complete();
         });
         async.awaitSuccess(3000);
     }
 
     //having a secret is equal to an ongoing bootstrapping process
-    public void testDeviceWithExistingSecret(TestContext context){
+    public void testDeviceWithExistingSecret(TestContext context) {
         Properties prop = new Properties();
         prop.setProperty("initialUser", "devicebootstrap");
         prop.setProperty("initialPassword", "Fhdt1bb1f");
