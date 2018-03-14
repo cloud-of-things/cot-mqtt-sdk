72cc51215a738ecbdaffa49fd8272dc18c462c2a
M Selchow <m.selchow@tarent.de>
2018-03-14 10:38:24 +0100

    fixes

--- src/system-test/java/de/tarent/telekom/cot/mqtt/SystemTest.java
+++ src/system-test/java/de/tarent/telekom/cot/mqtt/SystemTest.java
@@ -57,13 +57,7 @@ public class SystemTest {
 
     @After
     public void after() {
-        Set<String> list = vertx.deploymentIDs();
-        if (list != null && list.size() > 0) {
-            list.forEach(id -> {
-                logger.info("to undeploy:" + id);
-                vertx.undeploy(id);
-            });
-        }
+        vertx.undeploy(helper.deploymentID());
     }
 
     @Test
