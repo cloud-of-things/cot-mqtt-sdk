1af0539ae384ff10005a8e0b4f49765d976f8c3f
Desiree Amling <d.amling@tarent.de>
2018-03-26 17:11:09 +0200

    Refactored the ManagedObjectHelperVerticle class a bit to make it easier to understand. Also did a bit of refactoring to the MessageVerticle and MQTTHelper classes by renaming some static fields correctly and adding a couple more.

--- src/main/java/de/tarent/telekom/cot/mqtt/ManagedObjectHelperVerticle.java
+++ src/main/java/de/tarent/telekom/cot/mqtt/ManagedObjectHelperVerticle.java
@@ -27,9 +27,7 @@ public class ManagedObjectHelperVerticle extends AbstractVerticle {
     public void start() throws Exception {
         eb = vertx.eventBus();
 
-        eb.consumer("createManagedObject", msg -> {
-            createManagedObject((JsonObject) msg.body());
-        });
+        eb.consumer("createManagedObject", msg -> createManagedObject((JsonObject) msg.body()));
     }
 
     private void createManagedObject(JsonObject msg) {
@@ -50,54 +48,69 @@ public class ManagedObjectHelperVerticle extends AbstractVerticle {
         client.publishHandler(h -> {
             LOGGER.info("Message with topic " + h.topicName() + " with QOS " + h.qosLevel().name() + " received");
             if (h.topicName().equals(msg.getString(MO_SUBSCRIBE_TOPIC_KEY))) {
+                final String[] parsedPayload = SmartREST.parseResponsePayload(h.payload());
 
-                String[] parsedPayload = SmartREST.parseResponsePayload(h.payload());
-                //object doesnt exist
+                //object doesn't exist
                 if (parsedPayload[0].equals("50") && parsedPayload[2].equals("404")) {
-                    String message = SmartREST.getPayloadSelfCreationRequest(msg.getString(XID_KEY), msg.getString(DEVICE_ID_KEY),
-                        DEVICE_NAME_KEY);
-                    MOPublish(client, msg.getString(MO_PUBLISH_TOPIC_KEY), message);
+                    handleMODoesNotExist(msg);
                 }
+
                 //object already exists
                 else if (parsedPayload[0].equals("601")) {
-                    //601,1,mascot3,2817383;
+                    //601,1,mascot3,2817383;  NOSONAR - This isn't code...
                     //what to do when object for iccid already exists?
                     //TODO what does it mean, when the object already exists? how do we handle it?
                 }
+
                 //object created we
                 if (parsedPayload[0].equals("603")) {
-                    final JsonObject managedObject = new JsonObject();
-                    managedObject.put("managedObjectId", parsedPayload[2]);
-                    eb.publish("setConfig", managedObject);
-
-                    String registerICCIDString = SmartREST.getPayloadRegisterICCIDasExternalId(msg.getString(XID_KEY), parsedPayload[2], msg.getString("deviceId"));
-                    MOPublish(client, msg.getString(MO_PUBLISH_TOPIC_KEY), registerICCIDString);
-                    String updateOperationsString = SmartREST.getPayloadUpdateOperations(msg.getString(XID_KEY), parsedPayload[2]);
-                    MOPublish(client, msg.getString(MO_PUBLISH_TOPIC_KEY), updateOperationsString);
-                    client.unsubscribe(msg.getString(MO_SUBSCRIBE_TOPIC_KEY));
-                    client.disconnect();
-
-                    eb.publish("managedObjectCreated", managedObject);
+                    handleMOCreated(msg, parsedPayload[2]);
                 }
             }
         });
 
 
-        client.connect(port,  msg.getString(BROKER_URI_KEY), ch -> {
+        client.connect(port, msg.getString(BROKER_URI_KEY), ch -> {
             if (ch.succeeded()) {
                 LOGGER.info("Connected to a server");
-                client.subscribe(msg.getString(MO_SUBSCRIBE_TOPIC_KEY), MqttQoS.AT_MOST_ONCE.value(),
-                    d -> {
-                    });
-                MOPublish(client, msg.getString(MO_PUBLISH_TOPIC_KEY), SmartREST.getPayloadCheckManagedObject("mascot-testdevices1", msg.getString(DEVICE_ID_KEY)));
+                client.subscribe(msg.getString(MO_SUBSCRIBE_TOPIC_KEY), MqttQoS.AT_MOST_ONCE.value(), d -> {
+                });
+                publishMO(client,
+                    msg.getString(MO_PUBLISH_TOPIC_KEY),
+                    SmartREST.getPayloadCheckManagedObject("mascot-testdevices1", msg.getString(DEVICE_ID_KEY)));
             } else {
                 LOGGER.error("Failed to connect to a server", ch.cause());
             }
         });
     }
 
+    private void handleMODoesNotExist(final JsonObject msg) {
+        final String message = SmartREST.getPayloadSelfCreationRequest(msg.getString(XID_KEY),
+            msg.getString(DEVICE_ID_KEY),
+            DEVICE_NAME_KEY);
+        publishMO(client, msg.getString(MO_PUBLISH_TOPIC_KEY), message);
+    }
 
-    private void MOPublish(MqttClient client, String topic, String message) {
+    private void handleMOCreated(final JsonObject msg, final String payloadValue) {
+        final JsonObject managedObject = new JsonObject();
+        managedObject.put("managedObjectId", payloadValue);
+        eb.publish("setConfig", managedObject);
+
+        final String registerICCIDString = SmartREST.getPayloadRegisterICCIDasExternalId(msg.getString(XID_KEY),
+            payloadValue, msg.getString("deviceId"));
+        publishMO(client, msg.getString(MO_PUBLISH_TOPIC_KEY), registerICCIDString);
+
+        final String updateOperationsString = SmartREST.getPayloadUpdateOperations(msg.getString(XID_KEY), payloadValue);
+        publishMO(client, msg.getString(MO_PUBLISH_TOPIC_KEY), updateOperationsString);
+
+        client.unsubscribe(msg.getString(MO_SUBSCRIBE_TOPIC_KEY));
+        client.disconnect();
+
+        eb.publish("managedObjectCreated", managedObject);
+    }
+
+
+    private void publishMO(final MqttClient client, final String topic, final String message) {
         client.publish(
             topic,
             Buffer.buffer(message),
